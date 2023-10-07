5b4d8f5dbb9ad520c84731e592b1c15680d879bd
Cole Parks <cparks@wpi.edu>
2023-10-07 15:50:00 -0400

    Added state machine, kinda broken atm

--- LedHatTest/src/main.cpp
+++ LedHatTest/src/main.cpp
@@ -1,35 +1,64 @@
 
 #include "main.h"
 
-
-vector<int> ledPins = {A0,A1,A2};
-
+// Runs once
 void setup() {
-  
-  Serial.begin(9600);
-
-}
-
-void loop() {
-  // pulsePWM(ledPins[0], 500);
-  // pulsePWM(ledPins[1], 500);
-  // pulsePWM(ledPins[2], 500);
-
+  // Serial.begin(9600);
   initDigitalOutputs(ledPins);
-
-  // allOn(ledPins);
-  // delay(1000);
-  // allOff(ledPins);
-  // delay(1000);
-  // blink(ledPins, 100);
-  // for(int i = 0; i < 50; i++){
-  //   circle(ledPins, 100);
-  // }
-
-  circle(ledPins, 100);
-  
+  initButtonISR(BUTTON_PIN_1);
 }
 
+// Runs repeatedly
+void loop() {
+  // Global state variable 
+  circle(ledPins, 100);
+  // stateMachine(hatState, ledPins);
+  // delay(100);
+
+}
+
+void stateMachine(int state, vector<int> pins){
+  switch (state) {
+    case ALL_OFF:
+      allOff(pins);
+      break;
+    case ALL_ON:
+      allOn(pins);
+      break;
+    case BLINK_ALL:
+      blink(pins, 100);
+      break;
+    case CIRCLE:
+      circle(pins, 100);
+      break;
+    case PULSE_0:
+      pulsePWM(pins[0], 500);
+      break;
+    case PULSE_1:
+      pulsePWM(pins[1], 500);
+      break;
+    case PULSE_2:
+      pulsePWM(pins[2], 500);
+      break;
+    default:
+      allOff(pins);
+      break;
+  }
+}
+
+// Interrupt Service Routine
+void button_ISR(){
+  hatState++;
+  hatState = hatState % numStates;
+}
+
+// Hook up ISR to pin passed
+void initButtonISR(int pin){
+  pinMode(pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pin), button_ISR, FALLING);
+}
+
+// Set all pins in vector to OUTPUT
 void initDigitalOutputs(vector<int> pins){
   for (int pin : pins) {
     pinMode(pin, OUTPUT);
--- LedHatTest/src/main.h
+++ LedHatTest/src/main.h
@@ -9,3 +9,24 @@ using namespace std;
 
 // Function Prototypes
 void initDigitalOutputs(vector<int> pins);
+void stateMachine(int state, vector<int> pins);
+void button_ISR();
+void initButtonISR(int pin);
+
+// int enum for states
+enum states {
+  ALL_OFF,
+  ALL_ON,
+  BLINK_ALL,
+  CIRCLE,
+  PULSE_0,
+  PULSE_1,
+  PULSE_2,
+
+  numStates
+};
+
+// Global Variables and Constants
+#define BUTTON_PIN_1 2
+int hatState = CIRCLE;
+vector<int> ledPins = {A0,A1,A2};
